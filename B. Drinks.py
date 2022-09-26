5f77981e11c30b8ef7bfc438cac2944c9ce40022
madhu-chitturi <106515693+madhu-chitturi@users.noreply.github.com>
2022-09-26 21:18:06 +0530

    B. Drinks.py

--- B. Drinks.py
+++ B. Drinks.py
@@ -1,4 +1,4 @@
-                                                                                B. Drinks
+'''                                                                                B. Drinks
 
 Little Vasya loves orange juice very much. That's why any food and drink in his kitchen necessarily contains orange juice. There are n drinks in his fridge, the volume fraction of orange juice in the i-th drink equals pi percent.
 
@@ -26,7 +26,7 @@ outputCopy
 
 
 
-SOLUTION <==============PYTHON====================>
+SOLUTION <==============PYTHON====================>'''
 
 a=int(input())
 b=list(map(int,input().strip().split()))
