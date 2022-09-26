97b47489cbbd3d31d47bb11520d1df5771f9e465
madhu-chitturi <106515693+madhu-chitturi@users.noreply.github.com>
2022-09-26 20:14:22 +0530

    A. Domino piling.py

--- A. Domino piling.py
+++ A. Domino piling.py
@@ -1,8 +1,5 @@
-                                                      A. Domino piling
-                                                  time limit per test2 seconds
-                                                 memory limit per test256 megabytes
-                                                  inputstandard input
-                                                    outputstandard output
+'''                                                      A. Domino piling
+                                                  
 You are given a rectangular board of M × N squares. Also you are given an unlimited number of standard domino pieces of 2 × 1 squares. You are allowed to rotate the pieces. You are asked to place as many dominoes as possible on the board so as to meet the following conditions:
 
 1. Each domino completely covers two squares.
@@ -26,7 +23,7 @@ output
 4
 
 
-SOLUTION<============PYTHON============>
+SOLUTION<============PYTHON============>'''
 
 a,b=map(int,input().split())
 print((a*b)//2)
